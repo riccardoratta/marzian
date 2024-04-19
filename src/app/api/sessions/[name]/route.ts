c415077a1fb1e6fb8c169fc472dc9dfd81e30282
Riccardo Ratta <riccardoratta@gmail.com>
2024-04-19 12:16:12 +0200

    Add session DELETE

--- src/app/api/sessions/[name]/route.ts
+++ src/app/api/sessions/[name]/route.ts
@@ -20,7 +20,7 @@ export async function GET(
 
   if (spawnRes.status !== 0) {
     // Session not found error
-    if (spawnRes.stderr.toString().trim().startsWith("can't find pane")) {
+    if (spawnRes.stderr.toString().trim().startsWith("can't find")) {
       return NextResponse.json({}, { status: 404 });
     }
 
@@ -37,4 +37,33 @@ export async function GET(
   return NextResponse.json({
     lines: spawnRes.stdout.toString().split("\n"),
   });
+}
+
+export async function DELETE(
+  _: NextRequest,
+  { params }: { params: { name: string } }
+): Promise<NextResponse<SpawnError | {}>> {
+  const { name } = params;
+
+  const spawnRes = spawnSync(`tmux kill-session -t ${name}`, {
+    shell: true,
+  });
+
+  if (spawnRes.status !== 0) {
+    // Session not found error
+    if (spawnRes.stderr.toString().trim().startsWith("can't find")) {
+      return NextResponse.json({}, { status: 404 });
+    }
+
+    // General error
+    return NextResponse.json(
+      {
+        exitCode: spawnRes.status ?? -1,
+        stderr: spawnRes.stderr.toString(),
+      },
+      { status: 500 }
+    );
+  }
+
+  return NextResponse.json({});
 }

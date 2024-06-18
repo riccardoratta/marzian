103632fdf955503c68502b0170d27d64d6645c49
Riccardo Ratta <riccardoratta@gmail.com>
2024-06-18 17:11:15 +0200

    Fix skeleton values

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -79,7 +79,7 @@ function ListSkeleton() {
     <>
       <Flex my={23.5} mx={15} gap={20}>
         <Group flex={1}>
-          <Skeleton height={10} width={45} />
+          <Skeleton height={10} width={90} />
           <Skeleton height={10} width={80} />
         </Group>
         <Skeleton height={10} width={80} />
@@ -87,7 +87,7 @@ function ListSkeleton() {
       </Flex>
       <Flex my={23.5} mx={15} gap={20}>
         <Group flex={1}>
-          <Skeleton height={10} width={65} />
+          <Skeleton height={10} width={10} />
           <Skeleton height={10} width={80} />
         </Group>
         <Skeleton height={10} width={80} />

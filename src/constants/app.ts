edc4635b4a292148512c4a1543db3e3bdf8b3bec
Riccardo Ratta <riccardoratta@gmail.com>
2024-10-30 12:15:19 +0100

    Update version

--- src/constants/app.ts
+++ src/constants/app.ts
@@ -2,7 +2,7 @@ import type { MantineColorScheme, MantineBreakpoint } from "@mantine/core";
 import type { ThemeState } from "@/store/theme";
 
 /** @see https://semver.org */
-export const VERSION = "1.0.0";
+export const VERSION = "1.1.0";
 export const NAME = "App";
 export const DEFAULT_COLOR_SCHEME: MantineColorScheme = "auto";
 export const DEFAULT_THEME_ID: ThemeState["theme"]["id"] = "app-default";
